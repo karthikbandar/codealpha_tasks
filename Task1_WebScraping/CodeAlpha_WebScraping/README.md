fe9baf3187bdb893b2dbb13da2d76e66ff041c28
karthikbandar <karthikadapa9@gmail.com>
2026-08-27 16:58:32 +0530

    Task 1 Web Scraping files

---
+++ Task1_WebScraping/CodeAlpha_WebScraping/README.md
@@ -0,0 +1,15 @@
+# CodeAlpha Web Scraping Project
+
+## Project Title
+Book Data Web Scraper
+
+## Description
+This project scrapes book information from https://books.toscrape.com using Python.
+
+## Libraries Used
+- requests
+- beautifulsoup4
+- pandas
+
+## Output
+The scraped data is saved as books.csv.
---
+++ Task1_WebScraping/CodeAlpha_WebScraping/web_scraper.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://books.toscrape.com/"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+books = []
+
+for book in soup.find_all("article", class_="product_pod"):
+    title = book.h3.a["title"]
+    price = book.find("p", class_="price_color").text
+    availability = book.find("p", class_="instock availability").text.strip()
+    rating = book.p["class"][1]
+
+    books.append({
+        "Title": title,
+        "Price": price,
+        "Availability": availability,
+        "Rating": rating
+    })
+
+df = pd.DataFrame(books)
+
+df.to_csv("books.csv", index=False)
+
+print(df)
